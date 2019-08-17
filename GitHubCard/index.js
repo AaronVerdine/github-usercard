6ae93d1fe3efa38852ba0c8a31ce090d981e98d3
Your name <a.m.verdine@gmail.com>
2019-08-16 23:26:52 -0400

    Update index.js with followerArray and forEach

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -32,7 +32,18 @@ axios.get('https://api.github.com/users/aaronverdine')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach( item => {
+  axios.get(`https://api.github.com/users/${followersArray[]}`)
+    .then( response => {
+      cardCreator(response.data)
+    })
+    .catch( err => {
+      console.log(err)
+    })
+})
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
